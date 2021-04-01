1bd170dc02639f6faae3a0af05bcf474d9262319
LettuceFoot <febrian.francione@gmail.com>
2021-04-01 14:01:33 -0400

    Initial Commit: Added Spring boot Starting files

---
+++ quickChart/src/main/java/com/quickChart/QuickChartApplication.java
@@ -0,0 +1,13 @@
+package com.quickChart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickChartApplication.class, args);
+	}
+
+}
---
+++ quickChart/src/test/java/com/quickChart/QuickChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickChart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickChartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
